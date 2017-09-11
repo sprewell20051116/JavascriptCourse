638d4d51005635062b8b4a184d440dd60d86863f
GIGIGUN <sprewell20051116@gmail.com>
2017-09-11 23:14:33 +0800

    Complete Subject 12

--- sprewell20051116/11_JavaScript_Asynchronous/app/index.js
+++ sprewell20051116/11_JavaScript_Asynchronous/app/index.js
@@ -1,1 +1,22 @@
-console.log("This is the test Env for JavaScript Course");
+console.log("Subject 11 Promimse and Asynchronous");
+let p = new Promise((resolve, reject) => {
+  // resolve('Resolved promise data');
+  // reject("rejected promise data"); //Uncaught (in promise) rejected promise data
+  setTimeout(() => resolve("resolved promise data"), 3000); // schedule 3 sec
+})
+
+p.then(response => console.log(response))
+  .catch(error => console.log(error)); // rejected promise data
+
+console.log("after promise consumption");
+
+
+// ================================================
+console.log("Subject 11 Class85 - APIs and ES6 Fetch");
+const root = "https://jsonplaceholder.typicode.com/posts/1";
+fetch(root, {
+  method: "GET"
+})
+  // .then(response => console.log(response));
+  .then(response => response.json())
+  .then(json => console.log(json)); // full data
