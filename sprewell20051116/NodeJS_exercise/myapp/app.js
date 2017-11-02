8b3ba120177b7deb92e5b24695b9aefbbc3040d1
GIGIGUN <sprewell20051116@gmail.com>
2017-11-02 22:42:45 +0800

    Follow mongo w/ nodejs tutorial

--- sprewell20051116/NodeJS_exercise/myapp/app.js
+++ sprewell20051116/NodeJS_exercise/myapp/app.js
@@ -1,7 +1,20 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// // define routs here...
+// var server = app.listen(5050, function() {
+//   console.log('NodeJS server is running');
+// })
 
-// define routs here...
-var server = app.listen(5050, function() {
-  console.log('NodeJS server is running');
-})
+var mongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// connection url
+var url = 'mongodb://localhost:27017/myproject'
+
+// Use conect method to connect to the server
+mongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log('connection successfully to server');
+  db.close();
+});
