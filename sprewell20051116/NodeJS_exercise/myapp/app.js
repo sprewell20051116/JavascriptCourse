920726d82889086433b55a958e28216e38395eb9
GIGIGUN <sprewell20051116@gmail.com>
2017-11-02 23:05:50 +0800

    Add insert method for MongoDB

--- sprewell20051116/NodeJS_exercise/myapp/app.js
+++ sprewell20051116/NodeJS_exercise/myapp/app.js
@@ -16,5 +16,24 @@ var url = 'mongodb://localhost:27017/myproject'
 mongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log('connection successfully to server');
-  db.close();
+  // db.close();
+  insertDocuments(db, function() {
+    db.close();
+  });
 });
+
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // insert some documents
+  collection.insertMany( [
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Insert 3 documents into the collection");
+    callback(result);
+  })
+}
